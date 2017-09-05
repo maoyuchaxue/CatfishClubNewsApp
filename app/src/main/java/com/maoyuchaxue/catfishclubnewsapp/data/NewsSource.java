1164419a5f5c7f52bd1c6eb961583f4fdcc9a5c4
YU_Jason <yuzhijingcheng1996@hotmail.com>
2017-09-06 01:06:27 +0800

    #1. Fundamental structures of WebNewsSource are added.

--- app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/NewsSource.java
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/NewsSource.java
@@ -8,6 +8,15 @@ import com.maoyuchaxue.catfishclubnewsapp.data.exceptions.NewsSourceException;
  */
 
 public interface NewsSource {
+    /**
+     * Queries in the source a list of news based on the specified conditions.<br>
+     *     The lists are paged according to the page size returned by <code>getPageSize()</code>.
+     * @param pageNo The requested page number (starting from 1).
+     * @param category The category tag to be queried. If <code>null</code> is given,
+     *                 no restriction in the category tag of the news shall be assumed.
+     * @param keyword The keyword in news titles to be queried. If <code>null</code> is given,
+     *                no restriction in news titles shall be assumed.
+     * */
     NewsMetaInfo[] getNewsMetaInfoList(int pageNo, String keyword, NewsCategoryTag category) throws NewsSourceException;
     NewsContent getNewsContent(String id) throws NewsSourceException;
     int getPageSize();
--- app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/WebNewsSource.java
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/WebNewsSource.java
@@ -2,28 +2,97 @@ package com.maoyuchaxue.catfishclubnewsapp.data;
 
 import com.maoyuchaxue.catfishclubnewsapp.data.exceptions.NewsSourceException;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Created by YU_Jason on 2017/9/5.
  */
 
 public class WebNewsSource implements NewsSource {
+    private static int PAGE_SIZE = 20;
+    private String queryUrl, detailUrl;
+
+    /**
+     * @param queryUrl The URL for the query service API.
+     * @param detailUrl The URL for the detail service API.
+     *
+     * */
+    public WebNewsSource(String queryUrl, String detailUrl){
+        this.queryUrl = queryUrl;
+        this.detailUrl = detailUrl;
+    }
+
+    private String buildQueryString(int pageNo, String keyword, NewsCategoryTag category){
+        StringBuilder queryStr = new StringBuilder(queryUrl);
+        queryStr.append("?pageSize=" + PAGE_SIZE);
+        queryStr.append("&pageNo=" + pageNo);
+        if(keyword != null)
+            queryStr.append("&keyword=" + keyword);
+        if(category != null)
+            queryStr.append("&category=" + category.getIndex());
+
+        return queryStr.toString();
+    }
+
+    private String buildDetailString(String id){
+        return detailUrl + "?newsId=" + id;
+    }
+
+
     @Override
-    public NewsMetaInfo[] getNewsMetaInfoList(int pageNo, String keyword, NewsCategoryTag category) throws NewsSourceException {
+    public NewsMetaInfo[] getNewsMetaInfoList(int pageNo, String keyword,
+                                              NewsCategoryTag category) throws NewsSourceException {
+        try {
+            URL url = new URL(buildQueryString(pageNo, keyword, category));
+
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+
+            //TODO: manipulate the returned json data
+
+
+            con.disconnect();
+        } catch(IOException e){
+            // TODO: a more elaborate exception with details
+            throw new NewsSourceException();
+        }
+
         return new NewsMetaInfo[0];
     }
 
+    /**
+     *
+     *
+     * */
     @Override
     public NewsContent getNewsContent(String id) throws NewsSourceException {
+        try{
+            URL url = new URL(buildDetailString(id));
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+
+            //TODO: manipulate the returned json data to fetch the news content
+
+            con.disconnect();
+        } catch(IOException e){
+            //TODO: deal with the exception
+        }
         return null;
     }
 
     @Override
     public int getPageSize() {
-        return 0;
+        return PAGE_SIZE;
     }
 
     @Override
     public void close() throws NewsSourceException {
-
+        // simply do nothing
     }
 }
