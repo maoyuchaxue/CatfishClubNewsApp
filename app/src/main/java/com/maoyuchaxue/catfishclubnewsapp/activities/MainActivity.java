171386dd1cae6ac8c08d580aa4de987bb8ea1ae3
王纪霆 <wangjt15@mails.tsinghua.edu.cn>
2017-09-06 14:40:33 +0800

    增加了分类TabLayout/ViewPager

--- app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/activities/MainActivity.java
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/activities/MainActivity.java
@@ -1,16 +1,23 @@
 package com.maoyuchaxue.catfishclubnewsapp.activities;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.maoyuchaxue.catfishclubnewsapp.R;
+import com.maoyuchaxue.catfishclubnewsapp.controller.CategoryViewPagerAdapter;
 import com.maoyuchaxue.catfishclubnewsapp.fragments.NewsListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NewsListFragment.OnFragmentInteractionListener {
 
@@ -38,12 +45,34 @@ public class MainActivity extends AppCompatActivity implements NewsListFragment.
             }
         });
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_menu_tablayout);
 
-        Fragment newFragment = NewsListFragment.newInstance(-1, "", false);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_menu_viewpager);
 
-        transaction.replace(R.id.news_info_list, newFragment);
-        transaction.commit();
+
+        ArrayList<Fragment> fragments = new ArrayList<Fragment>();
+        fragments.add(NewsListFragment.newInstance(-1, "", false));
+        fragments.add(NewsListFragment.newInstance(1, "", false));
+        fragments.add(NewsListFragment.newInstance(2, "", false));
+        fragments.add(NewsListFragment.newInstance(3, "", false));
+
+        ArrayList<String> titles = new ArrayList<String>();
+        titles.add("ZH");
+        titles.add("C1");
+        titles.add("C2");
+        titles.add("C3");
+
+        CategoryViewPagerAdapter viewPagerAdapter = new CategoryViewPagerAdapter(getSupportFragmentManager(),
+                fragments, titles);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+//        Fragment newFragment = NewsListFragment.newInstance(-1, "", false);
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//
+//        transaction.replace(R.id.news_info_list, newFragment);
+//        transaction.commit();
     }
 
     @Override
---
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/controller/CategoryViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.maoyuchaxue.catfishclubnewsapp.controller;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by catfish on 17/9/6.
+ */
+
+public class CategoryViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mFragments;
+    private List<String> mTitles;
+
+    public CategoryViewPagerAdapter(FragmentManager fm, List<Fragment> fragments, List<String> titles) {
+        super(fm);
+        mFragments = fragments;
+        mTitles = titles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles.get(position);
+    }
+
+}
