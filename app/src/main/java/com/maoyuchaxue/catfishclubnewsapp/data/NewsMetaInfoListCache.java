70f082391ac6e63480df95920f059d6c98a5746f
Jason Yu <yuzhijingcheng1996@hotmail.com>
2017-09-07 16:05:46 +0800

    1. DatabaseNewsMetaInfoListCache implemented

--- app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/NewsMetaInfoListCache.java
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/NewsMetaInfoListCache.java
@@ -1,10 +1,13 @@
 package com.maoyuchaxue.catfishclubnewsapp.data;
 
 /**
+ * A cache does nothing. It just stores all those records it has encountered in a cache
+ * medium (e.g., a database or a file).
  * Created by YU_Jason on 2017/9/5.
  */
 
 public interface NewsMetaInfoListCache extends NewsMetaInfoListSource {
     NewsMetaInfo[] getNewsMetaInfoListFromCache(int pageNo, String keyword, NewsCategoryTag categoryTag);
+
     void flush();
 }
