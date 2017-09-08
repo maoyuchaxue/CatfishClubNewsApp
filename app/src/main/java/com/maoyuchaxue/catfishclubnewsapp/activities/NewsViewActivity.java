279395ae68094dfdbf7e38c52d7353b6653873c3
johnmmm <945841612@qq.com>
2017-09-08 16:57:19 +0800

    分享功能，还存在一些bug。

--- app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/activities/NewsViewActivity.java
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/activities/NewsViewActivity.java
@@ -21,6 +21,8 @@ import com.iflytek.cloud.SpeechSynthesizer;
 import com.iflytek.cloud.SynthesizerListener;
 import com.iflytek.cloud.SpeechError;
 
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 public class NewsViewActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
@@ -56,23 +58,53 @@ public class NewsViewActivity extends AppCompatActivity implements View.OnClickL
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.news_view_menu_share:
+                        //分享界面：
+                        //需要：
+                        //获得需要分享的链接，放到url里面
+                        //获得文章简介，放到setText里面
+
+                        OnekeyShare oks = new OnekeyShare();
+                        //关闭sso授权
+                        oks.disableSSOWhenAuthorize();
+
+                        // 分享时Notification的图标和文字  2.5.9以后的版本不     调用此方法
+                        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+                        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+                        oks.setTitle(getString(R.string.app_name));
+                        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+                        oks.setTitleUrl("http://sharesdk.cn");
+                        // text是分享文本，所有平台都需要这个字段
+                        oks.setText("分享新闻");
+                        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+                        //oks.setImagePath("storage/sdcard0/Download/15-26-52-u=3242651622,884328141&fm=27&gp=0.jpg");//确保SDcard下面存在此张图片
+                        oks.setImageUrl("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1504852209254&di=a0f05c8d58eb5cefdee04ac0fc0e4269&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F16%2F42%2F96%2F56e58PICAu9_1024.jpg");
+                        // url仅在微信（包括好友和朋友圈）中使用
+                        oks.setUrl("http://sharesdk.cn");
+                        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+                        oks.setComment("输入评论：");
+                        // site是分享此内容的网站名称，仅在QQ空间使用
+                        oks.setSite(getString(R.string.app_name));
+                        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+                        oks.setSiteUrl("http://sharesdk.cn");
+
+                        // 启动分享GUI
+                        oks.show(NewsViewActivity.this);
+
 //                        TODO: implement intent to share URL here
                         break;
                     case R.id.news_view_menu_voice:
                         //开始语音
-                        //1.创建SpeechSynthesizer对象, 第二个参数：本地合成时传InitListener
+                        //需要文章正文+文章内容，用String传入
 
                         SpeechSynthesizer mTts= SpeechSynthesizer.createSynthesizer(NewsViewActivity.this, ini);
-//2.合成参数设置，详见《科大讯飞MSC API手册(Android)》SpeechSynthesizer 类
+
                         mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan");//设置发音人
                         mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速
                         mTts.setParameter(SpeechConstant.VOLUME, "80");//设置音量，范围0~100
                         mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD); //设置云端
-//设置合成音频保存位置（可自定义保存位置），保存在“./sdcard/iflytek.pcm”
-//保存在SD卡需要在AndroidManifest.xml添加写SD卡权限
-//如果不需要保存合成音频，注释该行代码
-                        //mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH, "./sdcard/iflytek.pcm");
-//3.开始合成
+                        //设置合成音频保存位置（可自定义保存位置），保存在“./sdcard/iflytek.pcm”
+                        //保存在SD卡需要在AndroidManifest.xml添加写SD卡权限
+
                         String speaking = "大家好，我很皮诶。我买了一台iPhone 7。曾经，曾子My name is Van, I am an artist, a performance artist。";
                         mTts.startSpeaking(speaking, mSynListener);
 //                        TODO: implement voice here
