37de3389fe7099a83f4268e98c490bfb44c1b38c
YU_Jason <yuzhijingcheng1996@hotmail.com>
2017-09-09 16:44:42 +0800

    1. Name entity links added.

--- app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/WebNewsContentSource.java
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/data/WebNewsContentSource.java
@@ -1,5 +1,7 @@
 package com.maoyuchaxue.catfishclubnewsapp.data;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.maoyuchaxue.catfishclubnewsapp.data.exceptions.NewsSourceException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -12,6 +14,7 @@ import java.text.ParseException;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by YU_Jason on 2017/9/6.
@@ -23,6 +26,7 @@ public class WebNewsContentSource implements NewsContentSource {
             new SimpleDateFormat("yyyyMMddHHmmss"); // for parsing the date time
     private static final JsonParser JSON_PARSER = new JsonParser();
     private String apiUrl;
+    private static final String ENTITY_LINK_PREFIX = "https://baike.baidu.com/item/";
 
     public WebNewsContentSource(String apiUrl){
         this.apiUrl = apiUrl;
@@ -32,15 +36,38 @@ public class WebNewsContentSource implements NewsContentSource {
         return apiUrl + "?newsId=" + id;
     }
 
+    private void addNameEntities(JsonArray json, ArrayList<String> list){
+        for(JsonElement element : json)
+            list.add(element.getAsJsonObject().get("word").getAsString());
+    }
+
+    private String[] getNameEntities(JsonObject json){
+        ArrayList<String> list = new ArrayList<>();
+        addNameEntities(json.get("persons").getAsJsonArray(), list);
+        addNameEntities(json.get("organizations").getAsJsonArray(), list);
+        addNameEntities(json.get("locations").getAsJsonArray(), list);
+
+        return list.toArray(new String[0]);
+    }
 
     private NewsContent createContentFromJSON(JsonObject json) throws JsonParseException, ParseException{
         NewsContent content = new NewsContent();
 
-        content.setContentStr(json.get("news_Content").getAsString());
         content.setCrawlSource(json.get("crawl_Source").getAsString());
 //        content.setCrawTime(DATE_FORMAT.parse(json.get("crawl_Time").getAsString()));
         content.setCategory(json.get("news_Category").getAsString());
         content.setJournalist(json.get("news_Journal").getAsString());
+
+        String rawContent = json.get("news_Content").getAsString();
+        String[] nameEntities = getNameEntities(json);
+        StringBuilder contentWithLinks = new StringBuilder(rawContent);
+        for(String entity : nameEntities){
+            int index = contentWithLinks.indexOf(entity);
+            contentWithLinks.replace(index, index + entity.length(),
+                    "<a href=\"" + ENTITY_LINK_PREFIX + entity +
+                    "\">" + entity + "</a>");
+        }
+        content.setContentStr(contentWithLinks.toString());
 
         return content;
     }
