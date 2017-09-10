2f0591e352c97e04d1e79b96ca15d2ba8fe8ad94
johnmmm <945841612@qq.com>
2017-09-10 23:37:43 +0800

    垃圾夜间模式，只能改某些颜色。

--- app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/fragments/SettingsFragment.java
+++ app/src/main/java/com/maoyuchaxue/catfishclubnewsapp/fragments/SettingsFragment.java
@@ -1,18 +1,31 @@
 package com.maoyuchaxue.catfishclubnewsapp.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.maoyuchaxue.catfishclubnewsapp.R;
+import com.maoyuchaxue.catfishclubnewsapp.activities.MainActivity;
+
+import java.util.Set;
 
 /**
  * Created by catfish on 17/9/10.
  */
 
 public class SettingsFragment extends PreferenceFragment {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +36,27 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
-        
+
         return view;
     }
 
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference)
+    {
+        if(preference.getKey().equals("dark_style"))
+        {
+            if(AppCompatDelegate.getDefaultNightMode() == -1 || AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO)
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                getActivity().recreate();
+            }
+            else if(AppCompatDelegate.getDefaultNightMode() == -1 || AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                getActivity().recreate();
+            }
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 
 }
